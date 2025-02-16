f6eeadbb0442e6f65db7d3d65815b4ccdae33ebb
Shantanu Bhosale <shatanu.bhosale@bajajfinserv.in>
2025-02-17 01:08:07 +0530

    implemented mono repo with language translation

---
+++ packages/shared/globalError/index.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState, ReactNode } from "react";
+import { Alert,Platform } from "react-native";
+
+// Define the context type
+// Create the error context
+const ErrorContext = createContext(undefined);
+
+// Error Provider component
+export const GlobalErrorProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+
+  const setGlobalError = (errorMessage) => {
+    setError(errorMessage);
+    if (errorMessage) {
+        if(!Platform.OS === 'web'){
+            Alert.alert("Error", errorMessage, [{ text: "OK", onPress: () => setError(null) }]);
+        }else{
+            console.log('error happend')
+        }
+    }
+  };
+
+  return (
+    <ErrorContext.Provider value={{ error, setGlobalError }}>
+      {children}
+    </ErrorContext.Provider>
+  );
+};
+
+// Custom hook to use the error context
+export const useGlobalError = () => {
+  const context = useContext(ErrorContext);
+  if (!context) {
+    throw new Error("useGlobalError must be used within a GlobalErrorProvider");
+  }
+  return context;
+};
